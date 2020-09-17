bc3b94ac0d3b3af1da45e27cc139a4004a48ae13
unknown <shahanazshaik504@gmail.com>
2020-09-17 20:21:46 +0530

    sixth commit

--- myapp/views.py
+++ myapp/views.py
@@ -94,3 +94,15 @@ def webform(request):
             return render(request,'modelform.html',{'form':form})
     form=WebpageForm()
     return render(request,'modelform.html',{'form':form})
+
+def create_user(request):
+    if request.method=="POST":
+        user=UserModelForm(request.POST)
+        if user.is_valid():
+            password=user.cleaned_data['password']
+            user=user.save(commit=False)
+            user.set_password(password)
+            user.save()
+    form=UserModelForm()
+    return render (request,"modelform.html",{'form':form})
+    
