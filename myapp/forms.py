f1a2b954f221d76c494a8cb580f81ac2af6ab058
unknown <shahanazshaik504@gmail.com>
2020-09-28 20:19:27 +0530

    first commit

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from myapp.models import *
+from django.contrib.auth.models import User
 
 class TopicForm(forms.ModelForm):
     class Meta:
@@ -12,9 +13,9 @@ class WebpageForm(forms.ModelForm):
         fields=('topic','name','url')#'__all__'
         #exclude=('url',)
 
-from django.contrib.auth.models import User
 class UserModelForm(forms.ModelForm):
-    password=forms.CharField(max_length=30,widget=forms.PasswordInput)
+    password=forms.CharField(max_length=30,\
+        widget=forms.PasswordInput)
     class Meta:
-        model = User 
-        fields = ("username","password","email","is_staff","is_superuser")
+        model=User
+        fields=('username','password','email')
